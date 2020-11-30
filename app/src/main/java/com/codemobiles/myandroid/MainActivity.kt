a1f9218063bb7fba4d80f412df60f5203ffc2069
Pasit Chaisri <moowan2539@gmail.com>
2020-11-30 15:31:11 +0700

    basic_binding_ui

--- app/src/main/java/com/codemobiles/myandroid/MainActivity.kt
+++ app/src/main/java/com/codemobiles/myandroid/MainActivity.kt
@@ -2,13 +2,32 @@ package com.codemobiles.myandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.codemobiles.myandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setupEventWidget()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    private fun setupEventWidget() {
+        binding.loginButton.setOnClickListener {
+            val username = binding.usernameEditText.text.toString()
+            val password = binding.passwordEditText.text.toString()
+            Toast.makeText(applicationContext, "$username,$password", Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
+//life cycle
+
 
 
 // Build number(about,system) QSR1.190920.001
